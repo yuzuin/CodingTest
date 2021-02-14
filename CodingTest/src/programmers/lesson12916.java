b7aea4d21471d52487116415efafbe6e8bea9c19
yuzuin <united1878@hanmail.net>
2021-02-14 16:56:37 +0900

    다시

--- CodingTest/src/programmers/lesson12916.java
+++ CodingTest/src/programmers/lesson12916.java
@@ -1,5 +1,26 @@
 package programmers;
 
-public class lesson12916 {
 
+/*문자열 내 p와 y의 개수*/
+class lesson12916 {
+    boolean solution(String s) {
+        boolean answer = true;
+        
+        /*cnt만을 이용하여 ++와 --로 하는 방법도 있음..*/
+        int cntP = 0;
+        int cntY = 0;
+
+        for(int i = 0 ; i < s.length() ; i++){
+            if(s.charAt(i)=='p' || s.charAt(i)=='P'){
+                cntP++;        
+            }
+            if(s.charAt(i)=='y' || s.charAt(i)=='Y'){
+                cntY++;
+            }
+        }
+
+        answer = cntP==cntY;
+
+        return answer;
+    }
 }
